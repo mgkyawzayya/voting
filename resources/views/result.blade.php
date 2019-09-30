0e7c38296c608da499932daed60b30014792e25b
Kyaw Zayya <kyawzayya.ucsp@gmail.com>
2019-10-01 01:51:33 +0630

    fix logout

--- resources/views/result.blade.php
+++ resources/views/result.blade.php
@@ -58,7 +58,15 @@
                 </p>
               </a>
               <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdownProfile">
-                <a class="dropdown-item" href="#">Log out</a>
+                  <a class="dropdown-item" href="{{ route('logout') }}"
+                  onclick="event.preventDefault();
+                                document.getElementById('logout-form').submit();">
+                   {{ __('Logout') }}
+                </a>
+
+                <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                    @csrf
+                </form>
               </div>
             </li>
           </ul>
